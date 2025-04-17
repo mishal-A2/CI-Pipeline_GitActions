903cd545244b5783fe99ad66c499bd3fa2f1877a
Mishal-A <mishal45654@gmail.com>
2025-04-17 21:56:24 +0500

    Fix: Add newline at end of file to pass flake8

--- app.py
+++ app.py
@@ -1,9 +1,7 @@
-# app.py
 def main():
     print("Hello World from the Dockerized Python app!")
     print("by MishalAli 22i-1291")
 
-if __name__ == "__main__":
+if __name__ == "__main__":    
     main()
 
-    
